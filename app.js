bcde0d2a1f439de7f3281cb743efd95b4098ac8e
Philip Larie <philiprlarie@gmail.com>
2018-01-05 23:16:45 -0800

    add more linting

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require('express');
+
 var app = express();
-
 app.use(express.static('.'));
-
 app.listen(3000);
 console.log('Express server listening on port 3000');
--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+/* globals $, window */
+
 const $theForm = $('form');
-window.ALL_SETS_ARR.forEach((setName) => {
-  const [chapter, set] = setName.split('-')
+window.ALL_SETS_ARR.forEach(setName => {
+  const [chapter, set] = setName.split('-');
   let linkName = `Chapter ${parseInt(chapter)}, Set number ${parseInt(set)}`;
-  let url = `flashcard-set.html?sets[]=${setName}`
-  $theForm.append(`<div><input type="checkbox" value="${setName}" name="sets[]"><a href='${url}'>${linkName}</a></div>`);
+  let url = `flashcard-set.html?sets[]=${setName}`;
+  $theForm.append(
+    `<div><input type="checkbox" value="${setName}" name="sets[]"><a href='${url}'>${linkName}</a></div>`
+  );
 });
